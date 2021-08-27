add28c25d8a1f67f8fce61c0b6b37417fda38c58
kelvinaxhcar <kaxhcar@gmail.com>
2021-08-26 22:00:43 -0300

    Melhorando a interface

--- ClienteInvent/Program.cs
+++ ClienteInvent/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using ConsoleTables;
 
 namespace ClienteInvent
 {
@@ -13,7 +14,7 @@ namespace ClienteInvent
             Console.WriteLine("                 Bem vindo                        ");
             Console.WriteLine("--------------------------------------------------\n");
 
-            Console.WriteLine("Sistema de cadastro de clientes\n");
+            Console.WriteLine(" Sistema de cadastro de clientes\n");
             do
             {
             } while (Controle());
@@ -22,12 +23,13 @@ namespace ClienteInvent
 
         static bool Controle()
         {
-            Console.WriteLine("------------Digite um comando------------\n" +
-                              "Digite um comando e pressione enter \n\n" +
-                              "(I) Para incluir um novo cliente \n" +
-                              "(D) Para deletar cliente\n" +
-                              "(M) Para mostrar todos os clientes\n" +
-                              "(A) para alterar um cliente");
+            Console.WriteLine("------------- Digite um comando ------------------\n\n" +
+                              "Digite um comando e pressione enter. \n\n" +
+                              "(I)  Para incluir um novo cliente \n" +
+                              "(D)  Para deletar cliente\n" +
+                              "(M)  Para mostrar todos os clientes\n" +
+                              "(A)  para alterar um cliente\n");
+            Console.Write(" -->");
             var comando = Console.ReadLine();
             Console.Clear();
 
@@ -50,30 +52,42 @@ namespace ClienteInvent
                     return true;
                 case "A":
                     
-                    MostrarTabele();
-                    Console.Write("Digite um Id referente ao cliete que deseja alterar: ");
-                    clienteModel.id = Int32.Parse(Console.ReadLine());
-                    Console.Write("Digite um comando referente ao campo que deseja altarar: ");
-                    var comandoCampo = Int32.Parse(Console.ReadLine());
-
-                    Cliente clienteDoBanco = _repositorioCliete.BuscarPeloId(clienteModel.id);
-
-                    Cliente clienteEsitado = EditarCampos(comandoCampo, clienteDoBanco);
-
-
-                    _repositorioCliete.Update(clienteEsitado);
-
+                    AlterarCliente(clienteModel);
                     return true;
                 default:
                     return true;
             }
         }
 
+        private static void AlterarCliente(Cliente clienteModel)
+        {
+            MostrarTabele();
+            Console.Write("Digite um Id referente ao cliete que deseja alterar: \n");
+            clienteModel.id = Int32.Parse(Console.ReadLine());
+            Cliente clienteDoBanco = _repositorioCliete.BuscarPeloId(clienteModel.id);
+            Console.WriteLine("Digite um comando referente ao campo que deseja altarar: \n" +
+                              $"(1) Para Alterar o Nome: {clienteDoBanco.nome}\n" +
+                              $"(2) Para Alterar o CPF:  {clienteDoBanco.cpf}");
+            Console.WriteLine();
+            Console.Write(" -->");
+            var comandoCampo = Int32.Parse(Console.ReadLine());
+
+
+            Cliente clienteEsitado = EditarCampos(comandoCampo, clienteDoBanco);
+
+
+            _repositorioCliete.Update(clienteEsitado);
+            Console.Clear();
+            MostrarTabele();
+        }
+
         public static void InserirCliente(Cliente clienteModel)
         {
             Console.WriteLine("Didite o nome do cliente e pressione enter");
+            Console.Write( "-->");
             clienteModel.nome = Console.ReadLine();
             Console.WriteLine("Didite o CPF do cliente e pressione enter");
+            Console.Write( "-->");
             clienteModel.cpf = Console.ReadLine();
 
             if (clienteModel.nome != "" && clienteModel.cpf != "")
@@ -97,20 +111,27 @@ namespace ClienteInvent
 
         private static void MostrarTabele()
         {
-            Console.WriteLine("------------Cliente------------");
+            var table = new ConsoleTable("Id:", "Nome:", "CPF:");
+            
+            Console.WriteLine("------------- Lista de Clientes ---------------");
 
             foreach (var item in _repositorioCliete.ListarTodos())
             {
-                Console.WriteLine($"Id: {item.id} \t Nome: {item.nome} \t Cpf: {item.cpf}");
+                table.AddRow(item.id, item.nome, item.cpf);
             }
+            
+            table.Write();
+            Console.WriteLine();
 
-            Console.WriteLine("-------------------------------");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine();
         }
 
         private static void DeletarCliente(Cliente clienteModel)
         {
             Console.WriteLine("Didite o ID do cliente e pressione enter para deletar ou \n" +
                               "pressione somente enter para sair");
+            Console.Write( "-->");
             var idCliente = Console.ReadLine();
 
             var idClienteInt = 0;
@@ -148,12 +169,14 @@ namespace ClienteInvent
             switch (comando)
             {
                 case 1:
-                    Console.Write("Digite a nova informaçao e pressione enter: ");
+                    Console.WriteLine("Digite o novo nome e pressione enter: ");
+                    Console.Write( "-->");
                     cliente.nome = Console.ReadLine();
                     return cliente;
 
                 case 2:
-                    Console.Write("Digite a nova informaçao e pressione enter: ");
+                    Console.WriteLine("Digite o novo CPF e pressione enter: ");
+                    Console.Write( "-->");
                     cliente.cpf = Console.ReadLine();
                     return cliente;
 
